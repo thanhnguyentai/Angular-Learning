659ea8e9c28bc653a15580ff87866b32ff2be6ed
Thanh Nguyen <thanhnguyentai.vn@gmail.com>
2015-11-01 22:38:11 +0700

    Angular Gem Store part 1

---
+++ Angular-Gem-Store/Angular-Gem-Store/js/app.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var gems = [
+        {
+            name: 'Azurite',
+            price: 2.95,
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Azurite 2',
+            price: 3.05,
+            canPurchase: true,
+            soldOut: false
+        }
+    ];
+
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+})();
