c33c17714779c3ab9e259003556612978456eaa1
Thanh nguyen <thanhnguyentai.vn@gmail.com>
2016-03-17 17:25:02 +0700

    SportStore Application -- complete the first version

--- ProAngularJS/SportStore/controllers/sportStore.js
+++ ProAngularJS/SportStore/controllers/sportStore.js
@@ -1,6 +1,7 @@
 angular.module("sportStore")
 .constant("dataUrl", "http://localhost:5500/products")
-.controller("sportStoreCtrl", function($scope, $http, dataUrl){
+.constant("orderUrl", "http://localhost:5500/orders")
+.controller("sportStoreCtrl", function($scope, $http, $location, dataUrl, orderUrl, cart){
 	/*
 	$scope.data = {
 		products: [
@@ -19,4 +20,21 @@ angular.module("sportStore")
 		 .error(function(error){
 		 	$scope.data.error = error;
 		 });
+
+	$scope.sendOrder = function(shippingDetails){
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+
+		$http.post(orderUrl, order)
+			.success(function(data){
+				$scope.data.orderId = data.id;
+				cart.getProducts().length = 0;
+			})
+			.error(function(error){
+				$scope.data.orderError = error;
+			})
+			.finally(function(){
+				$location.path('/complete');
+			});
+	}
 });
